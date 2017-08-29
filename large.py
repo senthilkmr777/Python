631ff8b885471d5c8a249df4b60e552cf2a3cb0a
senthilkmr777 <31431219+senthilkmr777@users.noreply.github.com>
2017-08-29 12:25:47 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a = input('Enter the value of A: ')
+b = input('Enter the value of B: ')
+c = input('Enter the value of C: ')
+if (a >= b) and (a >= c):
+    print("A is largest number")
+elif (b >= a) and (b >= a):
+    print("B is largest number")
+else:
+    print("C is largest number")
